208ff12cb7db9a7c4c55ea0b392d6d117b55334f
Sumit_Hulsurkar <sumithulsurkar94@gmail.com>
2022-01-12 05:55:45 +0530

    Abstract class & Interface

---
+++ src/main/java/com/java/oops/AbstractVsInterface.java
@@ -0,0 +1,57 @@
+package com.java.oops;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+
+/**
+ * Interface Vs Abstract class
+ *
+ * Interface:
+ * 	- HAS A capability
+ * 	- Serializable as an Interface
+ * 	- Assume that we have two classes in your application,
+ * 	  which are implementing Serializable interface
+ *
+ * 		Employee implements Serializable
+ *
+ * 		Game implements Serializable
+ *
+ * 	- Here we can't establish any relation through Serializable interface between Employee and Game,
+ * 	- which are meant for different purpose.
+ * 	- Both are capable of Serializing the state and the comparasion ends there.
+ *
+ * Abstract class:
+ * 	- IS A relation
+ * 	- Reader is an abstract class.
+ * 	- BufferedReader is a Reader
+ * 	- FileReader is a Reader
+ * 	- FileReader and BufferedReader are used for common purpose : Reading data, and they are related through Reader class.
+ *
+ */
+public class AbstractVsInterface {
+
+    Reader a = new Reader() {
+        @Override
+        public int read(char[] cbuf, int off, int len) throws IOException {
+            return 0;
+        }
+
+        @Override
+        public void close() throws IOException {
+
+        }
+    };
+
+    FileReader f;
+
+    {
+        try {
+            f = new FileReader("s");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
