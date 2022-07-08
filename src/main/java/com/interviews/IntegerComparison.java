720ac490e2a7425c795f34b34ceb9f7c75a49361
Sumithulsurkar94@gmail.com <Sumithulsurkar94@gmail.com>
2022-07-08 13:40:34 +0530

    Wrapper Object Comparison

---
+++ src/main/java/com/interviews/IntegerComparison.java
@@ -0,0 +1,20 @@
+package com.interviews;
+
+public class IntegerComparison {
+    public static void main(String[] args) {
+
+        Integer i1 = 50;
+        Integer i2 = 50;
+        Integer i3 = 200;
+        Integer i4 = 200;
+
+        if(i1 == i2) {
+            System.out.println("i1 == i2");
+        } else
+            System.out.println("i1 != i2");
+        if(i3 == i4) {
+            System.out.println("i3 == i4");
+        } else
+            System.out.println("i3 != i4");
+    }
+}
