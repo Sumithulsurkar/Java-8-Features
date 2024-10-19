c7736871fbe8bbf1f3539febf91c998c30549333
Sumit_Hulsurkar <sumithulsurkar94@gmail.com>
2024-10-19 08:34:31 +0530

    inheritance

--- src/main/java/com/inheritance/MainInheritance.java
+++ src/main/java/com/inheritance/MainInheritance.java
@@ -4,12 +4,27 @@ public class MainInheritance {
 
     public static void main(String[] args) {
 
+
+        Parent parent1 = new Child();
+        parent1.show();
+
+        Child c = (Child) new Parent();// It will throw exception
+        c.show();
+
+        System.out.println("As Child is extending the Parent class, that's why Parent constructor is invoked first then followed by Child constructor & method ");
         Child ch = new Child();
         ch.show(); // child's method will be invoked
+
+        System.out.println("--------------------------");
 
         Parent p = new Parent();
         p.show(); // Parent's method will be invoked
 
+
+        System.out.println("--------------------------");
+
+        Parent parent = new Child();
+        parent.show(); // Child's method will be invoked
 
         //Parent Constructor Called
         //Child Constructor Called
